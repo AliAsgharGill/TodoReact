4ea76eb58ba91b15b10c60cbca8d0dd5cd6bd91b
aliasghargill <stylinalivlogs@gmail.com>
2023-11-06 20:17:54 +0500

    Initail Commit

---
+++ todo-react/src/App.js
@@ -0,0 +1,31 @@
+import * as React from "react";
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+
+import "./App.css";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <div>
+        <h1>Hello World</h1>
+        <Link to="about">About Us</Link>
+      </div>
+    ),
+  },
+  {
+    path: "about",
+    element: <div>About</div>,
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
